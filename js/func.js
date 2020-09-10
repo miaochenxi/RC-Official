a32edc51f3857df926f3a5e1bc308530ab256260
#Undefined_Mcx <miaochenxi@outlook.com>
2020-09-10 18:03:46 +0800

    修复滑动

--- js/func.js
+++ js/func.js
@@ -6,11 +6,14 @@ function onShow() {
   menu.style.left = "0px";
   sitemask.style.opacity = "50%";
   sitemask.style.visibility = "visible";
+  document.body.addEventListener("touchmove",unclickable,{passive:false});
 }
+function unclickable(e){e.preventDefault();}
 function onHide() {
   sitemask.style.opacity="0%";
   sitemask.style.visibility="hidden";
   menu.style.left="-100%";
+  document.body.removeEventListener("touchmove",unclickable);
 }
 document.getElementById("menu-btn").addEventListener("click", onShow);
 document.getElementById("sitemask").addEventListener("click", onHide);
